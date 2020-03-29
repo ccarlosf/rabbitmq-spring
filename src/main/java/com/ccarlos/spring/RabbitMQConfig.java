aa0b3156f32f29da257ed6ab53eb581d96d5d8d3
ccarlos <2238513100@qq.com>
2020-03-29 22:16:54 +0800

    4-2 SpringAMQP用户管理组件-RabbitAdmin应用

---
+++ src/main/java/com/ccarlos/spring/RabbitMQConfig.java
@@ -0,0 +1,30 @@
+package com.ccarlos.spring;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.ccarlos.spring.*"})
+public class RabbitMQConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses("127.0.0.1");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        connectionFactory.setVirtualHost("/");
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        return rabbitAdmin;
+    }
+}
--- src/test/java/com/ccarlos/spring/RabbitmqSpringApplicationTests.java
+++ src/test/java/com/ccarlos/spring/RabbitmqSpringApplicationTests.java
@@ -1,13 +1,26 @@
 package com.ccarlos.spring;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
 
 @SpringBootTest
 class RabbitmqSpringApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private RabbitAdmin rabbitAdmin;
+
+    @Test
+    public void testAdmin() throws Exception {
+
     }
 
 }
