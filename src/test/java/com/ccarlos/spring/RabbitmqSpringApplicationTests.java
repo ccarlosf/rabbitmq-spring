8372c0ec965da411f9ea87868cd5c2022533898d
ccarlos <2238513100@qq.com>
2020-03-31 17:30:29 +0800

    4-3 SpringAMQP用户管理组件-RabbitAdmin应用-2

--- src/test/java/com/ccarlos/spring/RabbitmqSpringApplicationTests.java
+++ src/test/java/com/ccarlos/spring/RabbitmqSpringApplicationTests.java
@@ -1,18 +1,21 @@
 package com.ccarlos.spring;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class RabbitmqSpringApplicationTests {
+public class RabbitmqSpringApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
     @Autowired
@@ -20,6 +23,40 @@ class RabbitmqSpringApplicationTests {
 
     @Test
     public void testAdmin() throws Exception {
+        rabbitAdmin.declareExchange(new DirectExchange("test.direct", false,
+                false));
+
+        rabbitAdmin.declareExchange(new TopicExchange("test.topic", false,
+                false));
+
+        rabbitAdmin.declareExchange(new FanoutExchange("test.fanout", false,
+                false));
+
+        rabbitAdmin.declareQueue(new Queue("test.direct.queue", false));
+
+        rabbitAdmin.declareQueue(new Queue("test.topic.queue", false));
+
+        rabbitAdmin.declareQueue(new Queue("test.fanout.queue", false));
+
+        rabbitAdmin.declareBinding(new Binding("test.direct.queue",
+                Binding.DestinationType.QUEUE,
+                "test.direct", "direct", new HashMap<>()));
+
+        rabbitAdmin.declareBinding(
+                BindingBuilder
+                        .bind(new Queue("test.topic.queue", false))        //直接创建队列
+                        .to(new TopicExchange("test.topic", false, false))    //直接创建交换机 建立关联关系
+                        .with("user.#"));    //指定路由Key
+
+
+        rabbitAdmin.declareBinding(
+                BindingBuilder
+                        .bind(new Queue("test.fanout.queue", false))
+                        .to(new FanoutExchange("test.fanout", false, false)));
+
+        //清空队列数据
+        rabbitAdmin.purgeQueue("test.topic.queue", false);
+
 
     }
 
